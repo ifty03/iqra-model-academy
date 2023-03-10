9d5cda562701f18e659a7843b8e339e8193276d8
ifty03 <ashikulislamb84@gmail.com>
2023-03-10 17:34:27 +0600

    our aim section added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Hero from "../../Components/Hero/Hero";
+import OurAim from "../../Components/OurAim/OurAim";
 import Overview from "../../Components/Overview/Overview";
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
     <div className="mb-20">
       <Hero />
       <Overview />
+      <OurAim />
     </div>
   );
 };
