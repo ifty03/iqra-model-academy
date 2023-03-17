c205b55a23cb1d032bfd53445a87ee0c4e01203d
ifty03 <ashikulislamb84@gmail.com>
2023-03-17 12:36:36 +0600

    backdrop blur added

--- src/Components/Overview/Overview.js
+++ src/Components/Overview/Overview.js
@@ -1,64 +1,72 @@
 import { Icon } from "@iconify/react";
 import React from "react";
+import OverviewModal from "./OverviewModal";
 
 const Overview = () => {
+  const [showModal, setShowModal] = React.useState(false);
   return (
-    <div className="grid lg:grid-cols-6 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 w-11/12 mx-auto md:-mt-24">
-      <div className=" py-10 group text-secondary bg-[#f7f5ed] border-x border-[#F7DC79] hover:bg-primary transition-colors duration-500 cursor-pointer">
-        <Icon
-          className="text-5xl mb-7 text-center w-fit mx-auto group-hover:text-white"
-          icon="fluent:hat-graduation-12-filled"
-        />
-        <p className="text-center font-semibold group-hover:text-white">
-          হিফজ রুটিন
-        </p>
+    <>
+      <div className="grid lg:grid-cols-6 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 w-11/12 mx-auto md:-mt-24">
+        <div
+          onClick={() => setShowModal(true)}
+          className=" py-10 group text-secondary bg-[#f7f5ed] border-x border-[#F7DC79] hover:bg-primary transition-colors duration-500 cursor-pointer"
+        >
+          <Icon
+            className="text-5xl mb-7 text-center w-fit mx-auto group-hover:text-white"
+            icon="fluent:hat-graduation-12-filled"
+          />
+          <p className="text-center font-semibold group-hover:text-white">
+            হিফজ রুটিন
+          </p>
+        </div>
+        <div className=" py-10 group text-secondary bg-[#f7f5ed] border-x border-[#F7DC79] hover:bg-primary transition-colors duration-500 cursor-pointer">
+          <Icon
+            className="text-5xl mb-7 text-center w-fit mx-auto group-hover:text-white"
+            icon="material-symbols:menu-book-rounded"
+          />
+          <p className="text-center font-semibold group-hover:text-white">
+            নাজেরা রুটিন
+          </p>
+        </div>
+        <div className=" py-10 group text-secondary bg-[#f7f5ed] border-x border-[#F7DC79] hover:bg-primary transition-colors duration-500 cursor-pointer">
+          <Icon
+            className="text-5xl mb-7 text-center w-fit mx-auto group-hover:text-white"
+            icon="uil:book-reader"
+          />
+          <p className="text-center font-semibold group-hover:text-white">
+            আমপারা রুটিন
+          </p>
+        </div>
+        <div className=" py-10 group text-secondary bg-[#f7f5ed] border-x border-[#F7DC79] hover:bg-primary transition-colors duration-500 cursor-pointer">
+          <Icon
+            className="text-5xl mb-7 text-center w-fit mx-auto group-hover:text-white"
+            icon="ph:book-bookmark-fill"
+          />
+          <p className="text-center font-semibold group-hover:text-white">
+            শিশু শ্রেণি রুটিন
+          </p>
+        </div>
+        <div className=" py-10 group text-secondary bg-[#f7f5ed] border-x border-[#F7DC79] hover:bg-primary transition-colors duration-500 cursor-pointer">
+          <Icon
+            className="text-5xl mb-7 text-center w-fit mx-auto group-hover:text-white"
+            icon="ph:bowl-food-fill"
+          />
+          <p className="text-center font-semibold group-hover:text-white">
+            খাবার রুটিন
+          </p>
+        </div>
+        <div className=" py-10 group text-secondary bg-[#f7f5ed] border-x border-[#F7DC79] hover:bg-primary transition-colors duration-500 cursor-pointer">
+          <Icon
+            className="text-5xl mb-7 text-center w-fit mx-auto group-hover:text-white"
+            icon="fluent:form-multiple-48-filled"
+          />
+          <p className="text-center font-semibold group-hover:text-white">
+            ভর্তি ফরম
+          </p>
+        </div>
       </div>
-      <div className=" py-10 group text-secondary bg-[#f7f5ed] border-x border-[#F7DC79] hover:bg-primary transition-colors duration-500 cursor-pointer">
-        <Icon
-          className="text-5xl mb-7 text-center w-fit mx-auto group-hover:text-white"
-          icon="material-symbols:menu-book-rounded"
-        />
-        <p className="text-center font-semibold group-hover:text-white">
-          নাজেরা রুটিন
-        </p>
-      </div>
-      <div className=" py-10 group text-secondary bg-[#f7f5ed] border-x border-[#F7DC79] hover:bg-primary transition-colors duration-500 cursor-pointer">
-        <Icon
-          className="text-5xl mb-7 text-center w-fit mx-auto group-hover:text-white"
-          icon="uil:book-reader"
-        />
-        <p className="text-center font-semibold group-hover:text-white">
-          আমপারা রুটিন
-        </p>
-      </div>
-      <div className=" py-10 group text-secondary bg-[#f7f5ed] border-x border-[#F7DC79] hover:bg-primary transition-colors duration-500 cursor-pointer">
-        <Icon
-          className="text-5xl mb-7 text-center w-fit mx-auto group-hover:text-white"
-          icon="ph:book-bookmark-fill"
-        />
-        <p className="text-center font-semibold group-hover:text-white">
-          শিশু শ্রেণি রুটিন
-        </p>
-      </div>
-      <div className=" py-10 group text-secondary bg-[#f7f5ed] border-x border-[#F7DC79] hover:bg-primary transition-colors duration-500 cursor-pointer">
-        <Icon
-          className="text-5xl mb-7 text-center w-fit mx-auto group-hover:text-white"
-          icon="ph:bowl-food-fill"
-        />
-        <p className="text-center font-semibold group-hover:text-white">
-          খাবার রুটিন
-        </p>
-      </div>
-      <div className=" py-10 group text-secondary bg-[#f7f5ed] border-x border-[#F7DC79] hover:bg-primary transition-colors duration-500 cursor-pointer">
-        <Icon
-          className="text-5xl mb-7 text-center w-fit mx-auto group-hover:text-white"
-          icon="fluent:form-multiple-48-filled"
-        />
-        <p className="text-center font-semibold group-hover:text-white">
-          ভর্তি ফরম
-        </p>
-      </div>
-    </div>
+      <OverviewModal setShowModal={setShowModal} showModal={showModal} />
+    </>
   );
 };
 
