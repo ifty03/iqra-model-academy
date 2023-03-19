dcfa9d520d972758b68c782e101c6e7fb34e8e97
ifty03 <ashikulislamb84@gmail.com>
2023-03-19 10:35:34 +0600

    modal responsive

--- src/Components/Overview/OverviewModal.js
+++ src/Components/Overview/OverviewModal.js
@@ -8,17 +8,16 @@ const OverviewModal = ({ showModal, setShowModal }) => {
         <div className="fixed top-0 left-0 right-0 bottom-0 backdrop-blur z-50">
           <>
             <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-              <div className="relative w-auto my-6 mx-auto max-w-6xl">
+              <div className="relative w-auto my-6 mx-auto max-w-2xl pt-[80vh]">
                 {/*content*/}
                 <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                   {/*header*/}
-                  <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                    <h3 className="text-3xl font-semibold">Modal Title here</h3>
+                  <div className="flex  justify-end p-5 border-b border-solid border-lightGray/30 rounded-t">
                     <button
-                      className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                      className="px-2  ml-auto bg-[#FF0000] rounded-full border-0 text-black float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
                       onClick={() => setShowModal(false)}
                     >
-                      <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      <span className=" text-white text-2xl outline-none focus:outline-none -mt-1">
                         ×
                       </span>
                     </button>
@@ -28,20 +27,19 @@ const OverviewModal = ({ showModal, setShowModal }) => {
                     <img className="w-full h-full" src={modalImg} alt="" />
                   </div>
                   {/*footer*/}
-                  <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                  <div className="flex items-center justify-end p-6 border-t border-solid border-lightGray/30 rounded-b">
                     <button
-                      className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                      className="text-white bg-[#FF0000] font-bold uppercase px-6 py-3 text-xs rounded-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                       type="button"
                       onClick={() => setShowModal(false)}
                     >
                       Close
                     </button>
                     <button
-                      className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                      className="bg-primary text-white  font-bold uppercase text-xs px-6 py-3 rounded-md mr-1 mb-1 ease-linear transition-all duration-150"
                       type="button"
-                      onClick={() => setShowModal(false)}
                     >
-                      Save Changes
+                      Download
                     </button>
                   </div>
                 </div>
