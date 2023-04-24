420e05887ff919971a88d2ae5900db6b5ba7c2ec
ifty03 <ashikulislamb84@gmail.com>
2023-04-23 23:46:35 -1200

    modify notfound page

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import Footer from "./Components/Footer/Footer";
 import NotFound from "./Components/NotFound/NotFound";
 import TopBar from "./Components/TopBar/TopBar";
 import Home from "./Pages/Home/Home";
+import key from "./adminKey";
 
 function App() {
   return (
     <div>
-      
       <TopBar />
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
+        <Route path={`/${key}`} element={<Home></Home>}></Route>
         <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
       <Footer />
---
+++ src/adminKey.js
@@ -0,0 +1,2 @@
+const key = "yeakub";
+export default key;
